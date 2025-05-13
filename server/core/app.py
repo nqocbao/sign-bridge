9b9eefc9c14c5fa38784e7c3bfc0953c2b397847
Do Tien Dat <dotiendat711@gmail.com>
2025-05-13 20:23:27 +0700

    feat: Them module xu ly data va chay model ra cac file

---
+++ server/core/app.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, jsonify, Response
+from flask_cors import CORS, cross_origin
+import tensorflow as tf
+import numpy as np
+import os
+import cv2
+import mediapipe as mp
+from pose_format import Pose
+from pose_format.pose_visualizer import PoseVisualizer
+from googletrans import LANGUAGES, Translator
+
+from mp_keypoints import mediapipe_detection, draw_styled_landmarks, extract_keypoints
+
+mp_holistic = mp.solutions.holistic 
+
+app = Flask(__name__)
+
+# CORS(app)
+CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
+
+model = tf.keras.models.load_model('../test.h5')
+actions = []
+with open("D:/TTNM/Sign-language-translator/server/data/words.txt", "r") as file:
+    actions = [line.strip() for line in file if line.strip()]
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json()
+    sequence = np.array(data['seq'])
+    res = model.predict(np.expand_dims(sequence, axis=0))[0]
+    print(actions[np.argmax(res)])
+    return jsonify({"word": actions[np.argmax(res)]})
+
+UPLOAD_FOLDER = './uploads'
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
